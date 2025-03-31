ea3029eaf1c27b7c2ae62b1fdd24b83e0a9e821b
54L <s.civiletti@gmail.com>
2025-03-31 15:00:20 +0200

    Lab.8 Ereditarietà, Enumerazione, Stream (Input/Output lettura e scrittura di File)

--- src/main/java/com/civiletti/stream/FileDiTestoInputConBufferedReaderDemo.java
+++ src/main/java/com/civiletti/stream/FileDiTestoInputConBufferedReaderDemo.java
@@ -1,38 +1,34 @@
 package com.civiletti.stream;
 
-import java.io.BufferedReader; // Importo la classe BufferedReader per la lettura efficiente del file.
-import java.io.FileReader; // Importo la classe FileReader per leggere il file riga per riga.
-import java.io.FileNotFoundException; // Importo la classe per gestire l'eccezione file non trovato.
-import java.io.IOException; // Importo la classe per gestire le eccezioni di input/output.
+import java.io.BufferedReader;        // classe BufferedReader per la lettura efficiente del file.
+import java.io.FileReader;            // classe FileReader per leggere il file riga per riga.
+import java.io.FileNotFoundException; // classe per gestire l'eccezione file non trovato.
+import java.io.IOException;           // classe per gestire le eccezioni di input/output.
 
 public class FileDiTestoInputConBufferedReaderDemo {
 
     public static void main(String[] args) {
 
         String nomeFile = "originale.txt"; // Nome del file da leggere
-
-        // Dichiarazione dell'oggetto BufferedReader per la lettura del file
-        BufferedReader inputStream = null;
-
+        BufferedReader inputStream = null; // Dichiaro l'oggetto BufferedReader per la lettura del file
         System.out.println("Il file " + nomeFile + "\ncontiene le righe seguenti:\n");
 
-        try { // Blocco try per l'apertura del file
+        try {
             inputStream = new BufferedReader(new FileReader(nomeFile)); // Apertura del file
-        } catch (FileNotFoundException e) { // Eccezione se il file non viene trovato
+        } catch (FileNotFoundException e) {
             System.out.println("Problema nell’apertura del file.");
         }
 
-        try { // Blocco try per la lettura del file riga per riga
-            String riga = inputStream.readLine(); // Legge la prima riga del file
+        try {
+            String riga = inputStream.readLine(); // Leggo la prima riga del file
 
-            // Loop per leggere tutte le righe fino a che non ci sono più dati
-            while (riga != null) {
+            while (riga != null) { // Ciclo per leggere tutte le righe finché non ci sono più dati
                 System.out.println(riga);
-                riga = inputStream.readLine(); // Legge la riga successiva
+                riga = inputStream.readLine(); // Leggo la riga successiva
             }
             inputStream.close(); // Chiusura dello stream di lettura
 
-        } catch (IOException e) { // Gestione dell'errore in caso di problemi di lettura
+        } catch (IOException e) {
             System.out.println("Errore nella lettura da " + nomeFile);
         }
     }
